6378fbd89de0f0325539fa4b39704e2d253e288f
Lizzie <koladeelizabeth01@gmail.com>
2022-05-04 04:16:42 -0700

    update

---
+++ main.js
@@ -0,0 +1,55 @@
+const initCanvas = (id) =>{
+    return  new fabric.Canvas(id, {
+        width: 500,
+        height: 500,
+        backgroundColor: 'skyblue'
+    });
+}
+
+const canvas = initCanvas('canvas');
+canvas.renderAll();
+
+
+//mouse:over
+
+canvas.on("mouse:over", (e)=>{
+    console.log(e)
+})
+
+document.getElementById("uploader").onchange = function(e) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var image = new Image();
+      image.src = e.target.result;
+      image.onload = function() {
+        var img = new fabric.Image(image);
+        img.set({
+          left: 100,
+          top: 60
+        });
+        img.scaleToWidth(200);
+        canvas.add(img).setActiveObject(img).renderAll();
+      }
+      image.onclick = function(e){
+        console.log('working')
+    }
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+  
+//zoom in and zoom out
+
+  canvas.on('mouse:down', function(e) {
+    if (e.target) {
+      console.log('an object was clicked! ', e.target.type);
+
+      canvas.setZoom(canvas.getZoom() * 1.1 )
+      
+    } else{
+        canvas.setZoom(canvas.getZoom() / 1.1 )
+    }
+  });
+  
+
+
+
